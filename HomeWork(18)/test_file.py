2b8835fb4af8f2bc0da0ee243551ecca28d10eba
zuevik <zuevik36@gmail.com>
2023-08-29 17:50:37 +0200

    HomeWork 17 in one folder

--- HomeWork(18)/test_file.py
+++
@@ -1,4 +0,0 @@
-from for_testing import square_in_file
-import pytest
-
-
--- HomeWork(18)/unc_file.py
+++
@@ -1,8 +0,0 @@
-def func(arguments):
-    sum = 0
-    for i in arguments:
-        sum += i
-    return sum
-
-
-print(func((1,2,3,4,5)))
